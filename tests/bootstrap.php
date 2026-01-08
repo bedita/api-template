c1c366b599d025cd1777f2b76294752db9a4157b
dante di domenico <dante@didoda.com>
2026-01-08 11:56:51 +0100

    fix: tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,38 +15,20 @@ declare(strict_types=1);
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
 
+use Cake\Cache\Cache;
 use Cake\Chronos\Chronos;
 use Cake\Core\Configure;
-use Cake\Datasource\ConnectionManager;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\ConnectionHelper;
+use Migrations\TestSuite\Migrator;
 
-/**
- * Test runner bootstrap.
- *
- * Add additional configuration/setup your application needs when running
- * unit tests in this file.
- */
 require dirname(__DIR__) . '/vendor/autoload.php';
-
 require dirname(__DIR__) . '/config/bootstrap.php';
+require dirname(__DIR__) . '/vendor/bedita/core/config/bootstrap.php';
 
 if (empty($_SERVER['HTTP_HOST']) && !Configure::read('App.fullBaseUrl')) {
     Configure::write('App.fullBaseUrl', 'http://localhost');
 }
-
-// DebugKit skips settings these connection config if PHP SAPI is CLI / PHPDBG.
-// But since PagesControllerTest is run with debug enabled and DebugKit is loaded
-// in application, without setting up these config DebugKit errors out.
-ConnectionManager::setConfig('test_debug_kit', [
-    'className' => 'Cake\Database\Connection',
-    'driver' => 'Cake\Database\Driver\Sqlite',
-    'database' => TMP . 'debug_kit.sqlite',
-    'encoding' => 'utf8',
-    'cacheMetadata' => true,
-    'quoteIdentifiers' => false,
-]);
-
-ConnectionManager::alias('test_debug_kit', 'debug_kit');
 
 // Fixate now to avoid one-second-leap-issues
 Chronos::setTestNow(Chronos::now());
@@ -56,19 +38,25 @@ Chronos::setTestNow(Chronos::now());
 // has been written to.
 session_id('cli');
 
+if (!defined('API_KEY')) {
+    define('API_KEY', 'API_KEY');
+}
+
 // Connection aliasing needs to happen before migrations are run.
 // Otherwise, table objects inside migrations would use the default datasource
 ConnectionHelper::addTestAliases();
 
-// Use migrations to build test database schema.
-//
-// Will rebuild the database if the migration state differs
-// from the migration history in files.
-//
-// If you are not using CakePHP's migrations you can
-// hook into your migration tool of choice here or
-// load schema from a SQL dump file with
-// use Cake\TestSuite\Fixture\SchemaLoader;
-// (new SchemaLoader())->loadSqlFiles('./tests/schema.sql', 'test');
+Cache::drop('_bedita_object_types_');
+Cache::setConfig('_bedita_object_types_', ['className' => 'Null']);
 
-// (new Migrator())->run();
+if (defined('UNIT_TEST_RUN')) {
+    Cache::clearAll();
+    $migrator = new Migrator();
+    $migrator->runMany([
+        ['plugin' => 'BEdita/Core'],
+        [],
+    ]);
+}
+
+Cache::clearAll();
+TableRegistry::getTableLocator()->clear();
