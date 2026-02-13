47d3ab38d149a7f9ed802c1bbc5767aad9ad78eb
Stefano Rosanelli <stefano.rosanelli@gmail.com>
2026-02-13 11:50:26 +0100

    fix: add basic ErrorController

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace MyApp\Controller;
+
+use BEdita\API\Controller\ErrorController as ErrorControllerBase;
+
+/**
+ * Error controller
+ */
+class ErrorController extends ErrorControllerBase
+{
+}
